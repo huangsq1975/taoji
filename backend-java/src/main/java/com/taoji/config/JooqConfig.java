566f909d8b68efb4a2180effe9d89a035991c026
huangsq <huangsq04@gmail.com>
2026-06-18 13:19:39 +0000

    Modify files

--- backend-java/src/main/java/com/taoji/config/JooqConfig.java
+++ backend-java/src/main/java/com/taoji/config/JooqConfig.java
@@ -1,12 +1,13 @@
 package com.taoji.config;
 
+import org.jooq.Converter;
+import org.jooq.ConverterProvider;
 import org.jooq.DSLContext;
-import org.jooq.ExecuteListenerProvider;
 import org.jooq.SQLDialect;
 import org.jooq.conf.Settings;
-import org.jooq.conf.StatementType;
 import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultConverterProvider;
 import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.jooq.tools.LoggerListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Configuration
 public class JooqConfig {
@@ -30,10 +33,27 @@ public class JooqConfig {
                 .withRenderFormatted(true)
                 .withExecuteLogging(true)
         );
-        // Log all SQL statements in dev
         config.setExecuteListenerProvider(
                 new DefaultExecuteListenerProvider(new LoggerListener())
         );
+        // Auto-convert java.sql.Timestamp → java.time.LocalDateTime for all records
+        config.setConverterProvider(new ConverterProvider() {
+            private final ConverterProvider delegate = new DefaultConverterProvider();
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public <T, U> Converter<T, U> provide(Class<T> tType, Class<U> uType) {
+                if (tType == Timestamp.class && uType == LocalDateTime.class) {
+                    return (Converter<T, U>) Converter.ofNullable(
+                            Timestamp.class,
+                            LocalDateTime.class,
+                            Timestamp::toLocalDateTime,
+                            LocalDateTime::toString
+                    );
+                }
+                return delegate.provide(tType, uType);
+            }
+        });
         return DSL.using(config);
     }
 }
