45a425184d7925312d99e8dc72a4236e5a5dc521
huangsq <huangsq04@gmail.com>
2026-06-18 11:28:21 +0000

    Delete files

--- backend/src/common/decorators/roles.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../../modules/users/user.entity';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
-
-export const PERMISSIONS_KEY = 'permissions';
-export const RequirePermissions = (...permissions: string[]) =>
-  SetMetadata(PERMISSIONS_KEY, permissions);
